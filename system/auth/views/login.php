b4c9445305103a9e288574d7c4e78618881424e3
Kanehira <eduardodkanehira@gmail.com>
2022-05-04 22:32:53 -0300

    Nova Página Inicial
    CSS e Menu Navbar com JS

--- system/auth/views/login.php
+++ system/auth/views/login.php
@@ -10,46 +10,93 @@ require ROOTPATH . '/lang/' . $sel_lang . "/auth/login.php";
 <!DOCTYPE html>
 <html lang="ptb">
 <head>
-  <title>Cadastro</title>
+  <title>Login</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="shortcut icon" href="/SEIA/media/icone_seia.svg"></link>
+  <link rel="stylesheet" href="/SEIA/style/header.css">
+  <link rel="stylesheet" href="/SEIA/style/body.css">  
+  <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
+  <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+
+<header class="header-branco">
+
+    <nav>
+
+      <img class="logo" src="/SEIA/media/logo_seia.svg"></img>
+
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+
+      <ul class="nav-list">
+        <!--<li><p>Esqueceu a senha? Então <a class='text-danger text-underline' href="index.php?action=passRecovery"><u>Recupere sua senha</u></a>!</p></li>-->
+        <a href="index.php?action=showTherms"><li class="criar-conta"><p>Criar conta</p></li></a>
+      </ul>
+
+    </nav>  
+
+</header><!--cabecalho-->
   
+<div class="corpo">
 
-<div class="col">
-    <div class="container">
-        <?php if(isset($data['error']) && $data['error']){ ?>
-            <div class="alert alert-danger" role="alert">
-            Nome de usuário ou senha errados.
-          </div>
-        <?php } ?>
+    <div class="col">
+
+        <div class="container">
+
+            <?php if(isset($data['error']) && $data['error']){ ?>
+                <div class="alert alert-danger" role="alert">
+                Nome de usuário ou senha errados.
+                </div>
+            <?php } ?>
+                
             
-        
-        <h2><?php echo $lang['login_header'];?></h2>
-        
-        <form action="index.php?action=login" method="post">
+            <h2><?php echo $lang['login_header'];?></h2>
 
-        <div class="form-group">
-            <label for="sigin_username"><?php echo $lang['login_username'];?></label>
-            <input type="text" class="form-control" id="sigin_username" name="sigin_username">
+            <div class="desenho-e-texto">
+            
+                <div class="area-login">
+
+                    <form action="index.php?action=login" method="post" class="formulario-login">
+
+                        <div class="form-group">
+                            <label for="sigin_username"><?php echo $lang['login_username'];?></label>
+                            <input type="text" class="form-control" id="sigin_username" name="sigin_username">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="sigin_pass"><?php echo $lang['login_pass'];?></label>
+                            <input type="password" class="form-control" id="sigin_pass" name="sigin_pass">
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                <?php echo $lang['login_button'];?>
+                            </button>
+                        </div>
+                    
+                    </form>
+
+                    <a class='text-danger text-underline' href="index.php?action=passRecovery">
+                        <p>Esqueceu nome de usuário ou senha?</p>
+                    </a>
+                
+                </div>
+
+            </div>
+
         </div>
 
-        <div class="form-group">
-            <label for="sigin_pass"><?php echo $lang['login_pass'];?></label>
-            <input type="password" class="form-control" id="sigin_pass" name="sigin_pass">
-        </div>
-
-        <button type="submit" class="btn btn-primary btn-lg btn-block"><?php echo $lang['login_button'];?></button>
-        </form>
     </div>
+
 </div>
-
-
 
 </body>
 </html>
--- system/auth/views/main.php
+++ system/auth/views/main.php
@@ -1,37 +1,77 @@
 
 <!DOCTYPE html>
 <html lang="ptb">
-<head>
-  <title>SEIA - Sistema de Ensino baseado em Inteligência Artificial</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</head>
-<body>
-  
-
-<div class="container" style="min-height:400px;">
-<div class="row">
-  <div class="col-sm-8 ">
-    <div class="container">
-      <h1>SEIA - Sistema de Ensino para Applied Behavior Analysis.</h1>
-      <p>Este sistema está em desenvolvimento na Universidade Federal do ABC. <u>Esta é a versão de testes.</u></p>
-               
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="jumbotron">
-      <p>Já possui cadastro? Então faça o <a href="index.php?action=loginForm">Login</a>!</p>
-      <p>Esqueceu a senha? Então <a class='text-danger text-underline' href="index.php?action=passRecovery"><u>Recupere sua senha</u></a>!</p>
-      <p> Não possui cadstro? <a href="index.php?action=showTherms">Cadastre-se aqui!</a></p>
-    </div>
-  </div>
-</div>
-</div>
     
+  <head>
+    <title>SEIA - Sistema de Ensino Baseado em Inteligência Artificial</title>
+    <link rel="shortcut icon" href="/SEIA/media/icone_seia.svg"></link>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/SEIA/style/header.css">
+    <link rel="stylesheet" href="/SEIA/style/body.css">
+    <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </head>
 
-</body>
+  <body> 
+    
+  <header>
+
+    <nav>
+
+      <img class="logo" src="/SEIA/media/logo_seia_branco.svg"></img>
+
+      <div class="mobile-menu">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+      </div>
+
+      <ul class="nav-list">        
+        <a href="index.php?action=loginForm"><li><p>Entrar</p></li></a>
+        <!--<li><p>Esqueceu a senha? Então <a class='text-danger text-underline' href="index.php?action=passRecovery"><u>Recupere sua senha</u></a>!</p></li>-->
+        <a href="index.php?action=showTherms"><li class="criar-conta"><p>Criar conta</p></li></a>
+      </ul>
+
+    </nav>  
+
+  </header><!--cabecalho-->
+
+<div class="corpo">
+
+    <div class="desenho-e-texto">
+
+      <div class="desenho">
+
+        <img src="/SEIA/media/vetores/balao.svg" class="balao"></img>
+        <img src="/SEIA/media/vetores/nuvens.svg" class="nuvens"></img>
+
+      </div>
+
+      <div class="texto-direita">
+
+        <h1>
+          Sobre o SEIA
+          <div class="sublinhado"></div>
+        </h1>
+
+        <p class="texto-conteudo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet tellus gravida, consectetur nisl in, ornare libero. Suspendisse lacinia felis tincidunt magna facilisis, eu lacinia urna finibus. Phasellus massa mi, laoreet quis convallis sit amet, vulputate ut tortor. Fusce condimentum orci et neque volutpat, id suscipit sem auctor.</p>
+      
+      </div>
+
+    </div>
+
+  </div> 
+
+<footer>
+
+  <p>Este sistema está em desenvolvimento na Universidade Federal do ABC. Esta é a versão de testes.</p>
+
+</footer> 
+
+  <script src="/SEIA/scripts/mobile-navbar.js"></script>
+
+  </body>
 </html>
